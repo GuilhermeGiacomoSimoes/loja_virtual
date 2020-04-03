3c3ebd659679b6a5c9f961e4a66ab3f9488d0507
Guilherme Giácomo Simões <49237332+GuilhermeGiacomoSimoes@users.noreply.github.com>
2020-04-02 21:05:43 -0300

    creating floating bar

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,20 +1,19 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    Widget _buildBodyBack() => Container(
+    Widget _buildBodyBack() => Container (
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            Color.fromARGB(255, 211, 118, 130), 
+            Color.fromARGB(255, 211, 118, 130),
             Color.fromARGB(255, 253, 181, 168)
           ],
           begin: Alignment.topLeft,
           end: Alignment.bottomRight
+
         )
       ),
     );
@@ -30,41 +29,10 @@ class HomeTab extends StatelessWidget {
               backgroundColor: Colors.transparent,
               elevation: 1.0,
               flexibleSpace: FlexibleSpaceBar(
-                title: const Text("novidade"),
+                title: const Text("Novidades"),
                 centerTitle: true,
               ),
-            ),
-
-            FutureBuilder<QuerySnapshot>(
-              future: Firestore.instance.collection("home").orderBy("pos").getDocuments(),
-              // ignore: missing_return
-              builder: (context, snapshot){
-                if(!snapshot.hasData){
-                  return SliverToBoxAdapter(
-                    child: Container(
-                      height: 200,
-                      alignment: Alignment.center,
-                      // ignore: missing_return
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                      ),
-                    ),
-                  );
-                }
-                else {
-                  print(snapshot.data.documents.length);
-                  return SliverToBoxAdapter(
-                    child: Container(
-                      height: 200,
-                      alignment: Alignment.center,
-                      // ignore: missing_return
-                      child: Container()
-                    ),
-                  );
-                }
-              },
-            ),
-
+            )
           ],
         )
       ],
